22c100d0b68f335c79483d3ec4d8526c9698132f
aoxiang <aoxiang@dipont.com>
2020-08-26 09:15:46 +0800

    update

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import store from './store';
 import router from './router';
 import Entry from './index.vue';
-import '@/assets/fonts/iconfont.js';
+// import '@/assets/fonts/iconfont.js';
 import vuetify from '@/plugins/vuetify';
 
 Vue.config.productionTip = false;
